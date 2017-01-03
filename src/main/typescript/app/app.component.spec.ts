62e4e5b70e27aeadd6d5b22f452bdd1e993ee23c
kucharzyk.tomasz <kucharzyk.tomasz@gmail.com>
2017-01-03 20:05:25 +0100

    feat(frontend): add vmware clarity library

--- src/main/typescript/app/app.component.spec.ts
+++ src/main/typescript/app/app.component.spec.ts
@@ -1,37 +1,46 @@
 /* tslint:disable:no-unused-variable */
-import {TestBed, async} from '@angular/core/testing';
-import {RouterTestingModule} from '@angular/router/testing';
+import {TestBed, async, ComponentFixture} from '@angular/core/testing';
 import {AppComponent} from './app.component';
+import {ClarityModule} from 'clarity-angular';
+import {RouterTestingModule} from '@angular/router/testing';
+
 
 describe('AppComponent', () => {
+
+  let fixture: ComponentFixture<AppComponent>;
+  let element: Element;
+  let component: AppComponent;
+
   beforeEach(() => {
     TestBed.configureTestingModule({
-      imports: [
-        RouterTestingModule
-      ],
       declarations: [
-        AppComponent
+        AppComponent,
       ],
+      imports: [
+        ClarityModule.forRoot(),
+        RouterTestingModule
+      ]
     });
-    TestBed.compileComponents();
+
+    fixture = TestBed.createComponent(AppComponent);
+    fixture.detectChanges();
+    element = fixture.nativeElement;
+    component = fixture.componentInstance;
+  });
+
+  afterEach(() => {
+    fixture.destroy();
   });
 
   it('should create the app', async(() => {
-    let fixture = TestBed.createComponent(AppComponent);
-    let app = fixture.debugElement.componentInstance;
-    expect(app).toBeTruthy();
+    expect(element).toBeTruthy();
   }));
 
   it(`should have as title 'shardis works!'`, async(() => {
-    let fixture = TestBed.createComponent(AppComponent);
-    let app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('shardis works!');
+    expect(component.title).toEqual('shardis works!');
   }));
 
   it('should render title in a h1 tag', async(() => {
-    let fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    let compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('shardis works!');
+    expect(element.querySelector('h1').textContent).toContain('shardis works!');
   }));
 });
--- src/main/typescript/app/app.module.ts
+++ src/main/typescript/app/app.module.ts
@@ -4,6 +4,7 @@ import {FormsModule} from '@angular/forms';
 import {HttpModule} from '@angular/http';
 import {AppRoutingModule} from './app-routing.module';
 import {AppComponent} from './app.component';
+import {ClarityModule} from 'clarity-angular';
 
 @NgModule({
   declarations: [
@@ -13,6 +14,7 @@ import {AppComponent} from './app.component';
     BrowserModule,
     FormsModule,
     HttpModule,
+    ClarityModule.forRoot(),
     AppRoutingModule
   ],
   providers: [],
