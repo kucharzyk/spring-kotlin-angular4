ce30d62bcf743a1db35b7e209c28833556052ea0
kucharzyk.tomasz <kucharzyk.tomasz@gmail.com>
2017-01-16 21:52:28 +0100

    fix(phantomjs): set DEFAULT_TIMEOUT_INTERVAL for jasmine

--- src/main/typescript/test.ts
+++ src/main/typescript/test.ts
@@ -10,7 +10,10 @@ import {BrowserDynamicTestingModule, platformBrowserDynamicTesting} from '@angul
 
 // Unfortunately there's no typing for the `__karma__` variable. Just declare it as any.
 declare var __karma__: any;
+declare var jasmine: any;
 declare var require: any;
+
+jasmine.DEFAULT_TIMEOUT_INTERVAL = 10000;
 
 // Prevent Karma from running prematurely.
 __karma__.loaded = function () {};
