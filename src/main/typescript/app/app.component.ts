9c0e037d11df01f698d2e1654e3a61022d6d1dae
kucharzyk.tomasz <kucharzyk.tomasz@gmail.com>
2017-01-28 12:15:26 +0100

    chore(deps): update clarity, compodoc and tslint

--- src/main/typescript/app/app.component.ts
+++ src/main/typescript/app/app.component.ts
@@ -1,11 +1,11 @@
-import {Component} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'shardis-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
 
   constructor() {
   }
