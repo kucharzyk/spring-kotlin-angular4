0ac9a6279ba04fe9cadd90f2719bb87a9568f1e6
kucharzyk.tomasz <kucharzyk.tomasz@gmail.com>
2017-02-02 18:52:37 +0100

    chore(frontend): update rxjs, clarity, karma, protractor + some fixes after angular-cli update

--- ngw.cmd
+++ ngw.cmd
@@ -1,6 +1,6 @@
 @echo off
 setlocal
-set NG_SCRIPT=%~dp0node_modules\angular-cli\bin\ng
+set NG_SCRIPT=%~dp0node_modules\@angular\cli\bin\ng
 if not exist %NG_SCRIPT% (
   echo Angular cli not found! Run gradle build first!
 ) else (
