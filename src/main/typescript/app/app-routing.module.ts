a24eed8ba8b74fb42cf46994388f148d825026a6
kucharzyk.tomasz <kucharzyk.tomasz@gmail.com>
2017-02-05 16:48:15 +0100

    fix(frontend): temporary use lazy loaded routes with aot (fixes #1)
    see: https://github.com/angular/angular-cli/issues/4192

--- src/main/typescript/app/app-routing.module.ts
+++ src/main/typescript/app/app-routing.module.ts
@@ -1,26 +1,16 @@
 import {NgModule} from '@angular/core';
 import {Routes, RouterModule} from '@angular/router';
-import {MainModule} from './main/main.module';
-import {AuthModule} from './auth/auth.module';
-
-export function loadMainModule() {
-  return MainModule;
-}
-
-export function loadAuthModule() {
-  return AuthModule;
-}
 
 export const routes: Routes = [
   {
     path: '',
     pathMatch: 'prefix',
-    loadChildren: loadMainModule
+    loadChildren: './main/main.module#MainModule'
   },
   {
     path: '',
     pathMatch: 'prefix',
-    loadChildren: loadAuthModule
+    loadChildren: './auth/auth.module#AuthModule'
   }
 ];
 
--- src/main/typescript/app/counter/counter.module.ts
+++ src/main/typescript/app/counter/counter.module.ts
@@ -1,15 +1,14 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import {CounterRoutingModule} from './counter-routing.module';
-import { CounterComponent } from './counter/counter.component';
-import { CounterPageComponent } from './counter-page/counter-page.component';
+import {NgModule} from '@angular/core';
+import {CommonModule} from '@angular/common';
+import {CounterComponent} from './counter/counter.component';
+import {CounterPageComponent} from './counter-page/counter-page.component';
 
 @NgModule({
   imports: [
-    CommonModule,
-    CounterRoutingModule
+    CommonModule
   ],
   declarations: [CounterComponent, CounterPageComponent],
   exports: [CounterComponent, CounterPageComponent]
 })
-export class CounterModule { }
+export class CounterModule {
+}
