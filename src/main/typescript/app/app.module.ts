79b08b0d7df1b55359f7219e4322cdad4da078c9
kucharzyk.tomasz <kucharzyk.tomasz@gmail.com>
2017-02-15 17:20:22 +0100

    feat(frontend): add ng2-webstorage package

--- src/main/typescript/app/app.module.ts
+++ src/main/typescript/app/app.module.ts
@@ -7,6 +7,7 @@ import {AppComponent} from './app.component';
 import {ClarityModule} from 'clarity-angular';
 import {CoreModule} from './core/core.module';
 import {MainModule} from './main/main.module';
+import {Ng2Webstorage} from 'ng2-webstorage';
 
 @NgModule({
   declarations: [
@@ -18,6 +19,7 @@ import {MainModule} from './main/main.module';
     ReactiveFormsModule,
     HttpModule,
     ClarityModule.forRoot(),
+    Ng2Webstorage.forRoot({prefix: 'shardis', separator: '.'}),
     AppRoutingModule,
     CoreModule,
     MainModule
