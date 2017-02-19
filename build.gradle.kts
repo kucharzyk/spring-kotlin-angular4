f550bd66ad176696d89d752f30c9df30b35b1a4a
kucharzyk.tomasz <kucharzyk.tomasz@gmail.com>
2017-02-19 19:49:43 +0100

    chore(core): set version to 0.0.6-SNAPSHOT

--- build.gradle.kts
+++ build.gradle.kts
@@ -9,7 +9,7 @@ import org.gradle.testing.jacoco.tasks.JacocoReport
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 group = "com.shardis"
-version = "0.0.5"
+version = "0.0.6-SNAPSHOT"
 
 buildscript {
 
