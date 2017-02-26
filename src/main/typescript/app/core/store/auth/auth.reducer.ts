6f92500c083223d5c3f5a394039f4df2d19f81d0
kucharzyk.tomasz <kucharzyk.tomasz@gmail.com>
2017-02-26 20:14:38 +0100

    feat(core): update angular-cli to 1.0.0-rc.0

--- src/main/typescript/app/core/store/auth/auth.reducer.ts
+++ src/main/typescript/app/core/store/auth/auth.reducer.ts
@@ -23,6 +23,7 @@ export function authReducer(state = initialState, action: Actions): AuthState {
     case ActionTypes.PROCESS_TOKEN:
       const processTokenAction: ProcessTokenAction = action as ProcessTokenAction;
       const userData = decodeAccessToken(processTokenAction.payload);
+      console.log('userdata is ', userData);
       return (!userData) ? state : Object.assign({}, initialState, {
           authenticated: true,
           tokenExpirationDate: new Date(userData.exp * 1000),
@@ -32,10 +33,10 @@ export function authReducer(state = initialState, action: Actions): AuthState {
 
     case ActionTypes.AUTH_ERROR:
       const authErrorAction: AuthErrorAction = action as AuthErrorAction;
-      return Object.assign({}, userData, {error: authErrorAction.payload});
+      return Object.assign({}, state, {error: authErrorAction.payload});
 
     case ActionTypes.CLEAR_AUTH_ERROR:
-      return Object.assign({}, userData, {error: null});
+      return Object.assign({}, state, {error: null});
 
     default:
       return state;
