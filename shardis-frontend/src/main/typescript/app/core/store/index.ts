676a83dc765b5b54a75b1528cdbc9510d09c1c5a
kucharzyk.tomasz <kucharzyk.tomasz@gmail.com>
2017-05-08 22:08:43 +0200

    chore(frontend): update ngrx-store-localstorage to 0.1.7

--- shardis-frontend/src/main/typescript/app/core/store/index.ts
+++ shardis-frontend/src/main/typescript/app/core/store/index.ts
@@ -1,10 +1,10 @@
 import {compose} from '@ngrx/core/compose';
 import {storeFreeze} from 'ngrx-store-freeze';
-import {combineReducers, ActionReducer} from '@ngrx/store';
+import {ActionReducer, combineReducers} from '@ngrx/store';
 import {CounterState} from './counter/counter.state';
 import {environment} from '../../../environments/environment';
 import {localStorageSync} from 'ngrx-store-localstorage';
-import {RouterState, routerReducer} from '@ngrx/router-store';
+import {routerReducer, RouterState} from '@ngrx/router-store';
 import {counterReducer} from './counter/counter.reducer';
 import {authReducer} from './auth/auth.reducer';
 import {AuthState} from './auth/auth.state';
@@ -24,7 +24,7 @@ const reducers = {
 
 const developmentReducer: ActionReducer<RootState> = compose(
   storeFreeze,
-  localStorageSync(['counter', 'router'], true),
+  localStorageSync({keys: ['counter', 'router'], rehydrate: true, removeOnUndefined: true}),
   combineReducers)(reducers);
 
 const productionReducer: ActionReducer<RootState> = compose(combineReducers)(reducers);
