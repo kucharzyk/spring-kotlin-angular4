938cb72e392385c2dc69219235d6f9fabffbde3c
kucharzyk.tomasz <kucharzyk.tomasz@gmail.com>
2017-06-12 20:11:21 +0200

    chore(core): enable sourcemaps in app

--- shardis-web/src/main/kotlin/com/shardis/support/view/resolvers/PushStateResourceResolver.kt
+++ shardis-web/src/main/kotlin/com/shardis/support/view/resolvers/PushStateResourceResolver.kt
@@ -12,7 +12,7 @@ import javax.servlet.http.HttpServletRequest
 class PushStateResourceResolver : ResourceResolver {
 
     private val index = ClassPathResource("/static/index.html")
-    private val handledExtensions = listOf("html", "js", "json", "csv", "css", "png", "svg", "eot", "ttf", "woff", "appcache", "jpg", "jpeg", "gif", "ico")
+    private val handledExtensions = listOf("html", "js", "json", "csv", "css", "png", "svg", "eot", "ttf", "woff", "appcache", "jpg", "jpeg", "gif", "ico","map")
     private val ignoredPaths = listOf("api")
 
     override fun resolveResource(request: HttpServletRequest, requestPath: String, locations: List<Resource>, chain: ResourceResolverChain): Resource? {
@@ -21,39 +21,34 @@ class PushStateResourceResolver : ResourceResolver {
 
     override fun resolveUrlPath(resourcePath: String, locations: List<Resource>, chain: ResourceResolverChain): String? {
         val resolvedResource = resolve(resourcePath, locations) ?: return null
-        try {
-            return resolvedResource.url.toString()
+        return try {
+             resolvedResource.url.toString()
         } catch (e: IOException) {
-            return resolvedResource.filename
+            resolvedResource.filename
         }
 
     }
 
-    private fun resolve(requestPath: String, locations: List<Resource>): Resource? {
-        return when {
+    private fun resolve(requestPath: String, locations: List<Resource>): Resource? = when {
             isIgnored(requestPath) -> null
             isHandled(requestPath) -> locations
                 .map { loc -> createRelative(loc, requestPath) }
-                .filter { resource -> resource != null && resource.exists() }
+                .filterNotNull()
+                .filter { it.exists() }
                 .firstOrNull()
             else -> index
         }
 
-    }
-
-    private fun createRelative(resource: Resource, relativePath: String): Resource? {
-        try {
-            return resource.createRelative(relativePath)
+    private fun createRelative(resource: Resource, relativePath: String): Resource? = try {
+            resource.createRelative(relativePath)
         } catch (e: IOException) {
-            return null
+            null
         }
-
-    }
 
     private fun isIgnored(path: String): Boolean = ignoredPaths.contains(path)
 
     private fun isHandled(path: String): Boolean {
         val extension = StringUtils.getFilenameExtension(path)
-        return handledExtensions.any { ext -> ext == extension }
+        return handledExtensions.any { it == extension }
     }
 }
