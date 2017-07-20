f6d6ff87ef53203f091e5a26f921b6eb26b357c9
kucharzyk.tomasz <kucharzyk.tomasz@gmail.com>
2017-07-20 20:30:02 +0200

    feat(frontend): update node to 8.2.0

--- shardis-frontend/build.gradle.kts
+++ shardis-frontend/build.gradle.kts
@@ -11,7 +11,7 @@ tasks.withType<Jar> {
 }
 
 configure<NodeExtension> {
-  version = "8.1.4"
+  version = "8.2.0"
   yarnVersion = "0.27.5"
   download = true
   workDir = file("${project.buildDir}/nodejs")
